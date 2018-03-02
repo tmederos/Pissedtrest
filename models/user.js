be118db0ffa8119a70f6573b7e16539bd7f75b0b
Trish Mederos <trishmederos@gmail.com>
2018-03-01 21:42:35 -0700

    created tests

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
     },
       email: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       validate: {
         len: [1]
       }

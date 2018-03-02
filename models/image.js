16e592e1bd3134b982748094b281a46bb6e23484
Grant Stoltz <grant@grantstoltz.com>
2018-03-01 18:27:41 -0700

    Updating HTML and client-side js

--- models/image.js
+++ models/image.js
@@ -11,14 +11,14 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [1]
       }
-    },
-    pin_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-     }
+    }
+    // pin_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1]
+    //   }
+    //  }
   });
   return Image;
 };

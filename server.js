5c311b7f08d54db93969d631f24116b9b6ed129c
Grant Stoltz <grant@grantstoltz.com>
2018-03-04 14:29:36 -0700

    Server.js changes

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "/auth/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   if (profile) {

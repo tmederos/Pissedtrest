6f1fc191ec3faa4174a707bd94354d4612675218
Trish Mederos <31010724+tmederos@users.noreply.github.com>
2018-03-12 14:07:38 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
  "development": {
    "username": process.env.DEV_DB_USERNAME || "root",
-   "password": "root",
-   "port": process.env.DEV_DB_PORT || 3308,
+   "password":null,
+   "port": process.env.DEV_DB_PORT || 3306,
    "database": "pin_db",
    "host": "127.0.0.1",
    "dialect": "mysql"
  },
  "test": {
    "username": "root",
-   "password": "root",
-   "port": 3308,
+-   "password": null,
+-   "port": 3306,
    "database": "database_test",
    "host": "127.0.0.1",
    "dialect": "mysql"
